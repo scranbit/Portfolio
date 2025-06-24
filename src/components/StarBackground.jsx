2f0b2b47b16a9e341ff5cbe1bf070d653a5bcea7
DevOPauloZ <paulodev2003@gmail.com>
2025-06-24 18:07:07 -0300

    menu created and finished

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -12,35 +12,15 @@ export const StarBackground = () => {
     generateMeteors();
 
     const handleResize = () => {
-      generateStars()
-    }
+      generateStars();
+    };
 
-    window.addEventListener('resize', handleResize)
+    window.addEventListener('resize', handleResize);
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const generateStars = () => {
-    const numberOfMeteors = 5;
-    const newMeteors = [];
-
-    for (let i = 0; i < numberOfMeteors; i++) {
-      newMeteors.push({
-        id: i,
-        size: Math.random() * 2 + 1,
-        x: Math.random() * 100,
-        y: Math.random() * 20,
-        delay: Math.random() * 15,
-        animationDuration: Math.random() * 3 + 3,
-      });
-
-      setMeteors(newMeteors);
-    }
-  };
-
-  const generateMeteors = () => {
     const numberOfStars = Math.floor(
       (window.innerWidth * window.innerHeight) / 10000,
     );
@@ -58,6 +38,24 @@ export const StarBackground = () => {
       });
 
       setStars(newStars);
+    }
+  };
+
+  const generateMeteors = () => {
+    const numberOfMeteors = 4;
+    const newMeteors = [];
+
+    for (let i = 0; i < numberOfMeteors; i++) {
+      newMeteors.push({
+        id: i,
+        size: Math.random() * 2 + 1,
+        x: Math.random() * 100,
+        y: Math.random() * 20,
+        delay: Math.random() * 15,
+        animationDuration: Math.random() * 3 + 3,
+      });
+
+      setMeteors(newMeteors);
     }
   };
   return (
