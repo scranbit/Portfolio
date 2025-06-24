18c021d730c8cae1e99c56e73ebf3b69616156f6
DevOPauloZ <paulodev2003@gmail.com>
2025-06-24 03:16:20 -0300

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+  return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    {/* Theme Toggle */}
+
+    {/* Backgrounf Effects */}
+
+    {/* Navbar */}
+
+    {/* Main Content */}
+
+    {/* Footer */}
+  </div>
+}
