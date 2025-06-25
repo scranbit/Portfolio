6a41303b67dc6a97c87a14a54f662808f9a982c9
DevOPauloZ <paulodev2003@gmail.com>
2025-06-24 23:13:33 -0300

    skills section finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,35 @@
-import { cn } from "@/lib/utils";
-import { Menu, X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { cn } from '@/lib/utils';
+import { Menu } from 'lucide-react';
+import { X } from 'lucide-react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const navItems = [
-  { name: "Home", href: "#hero" },
-  { name: "About", href: "#about" },
-  { name: "Skills", href: "#skills" },
-  { name: "Projects", href: "#projects" },
-  { name: "Contact", href: "#contact" },
+  { name: 'Home', href: '#hero' },
+  { name: 'About', href: '#about' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Contact', href: '#contact' },
 ];
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10);
     };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
   return (
     <nav
       className={cn(
-        "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        'fixed w-full z-40 transition-all duration-300',
+        isScrolled
+          ? 'py-3 bg-background/80 backdrop-blur-md shadow-xs'
+          : 'py-5',
       )}
     >
       <div className="container flex items-center justify-between">
@@ -35,7 +38,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> PauloTech </span>{" "}
+            <span className="text-glow text-foreground">PauloTech</span>{' '}
             Portfolio
           </span>
         </a>
@@ -52,24 +55,18 @@ export const Navbar = () => {
             </a>
           ))}
         </div>
-
         {/* mobile nav */}
-
-        <button
-          onClick={() => setIsMenuOpen((prev) => !prev)}
-          className="md:hidden p-2 text-foreground z-50"
-          aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
-        >
-          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
+        <button onClick={() => setIsMenuOpen((prev) => !prev )} className='md:hidden p-2 text-foreground z-50'
+          aria-label={isMenuOpen ? "Close Menu" : "Open Menu" }>
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
-
         <div
           className={cn(
-            "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
-            "transition-all duration-300 md:hidden",
+            'fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center',
+            'transition-all duration-300 md:hidden',
             isMenuOpen
-              ? "opacity-100 pointer-events-auto"
-              : "opacity-0 pointer-events-none"
+              ? 'opacity-100 pointer-events-auto'
+              : 'opacity-0 pointer-events-none',
           )}
         >
           <div className="flex flex-col space-y-8 text-xl">
