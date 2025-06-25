98e45caf16ff27f74b27836bf5ab87da60e23b75
DevOPauloZ <paulodev2003@gmail.com>
2025-06-25 01:59:33 -0300

    contact and footer finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Notfound } from "./pages/Notfound";
 import { Home } from "./pages/Home";
+import { Toaster } from '@/components/ui/toaster';
 function App() {
 
   return (
     <>
+    <Toaster />
     <BrowserRouter>
     <Routes>
       <Route index element={<Home />}/>
