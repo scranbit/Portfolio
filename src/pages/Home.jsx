05cf86a5431574cc71783430979197ba025996d7
DevOPauloZ <paulodev2003@gmail.com>
2025-06-24 21:53:48 -0300

    about me created and finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutMe";
 
 export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -11,7 +13,10 @@ export const Home = () => {
     {/* Navbar */}
     <Navbar />
     {/* Main Content */}
-
+    <main>
+      <HeroSection />
+      <AboutSection />
+    </main>
     {/* Footer */}
   </div>
 }
