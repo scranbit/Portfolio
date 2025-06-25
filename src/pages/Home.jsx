b3e7b6cdba94ec438897e7b5b0961965f091d605
DevOPauloZ <paulodev2003@gmail.com>
2025-06-25 00:49:43 -0300

    projects section finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutMe";
 import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
 
 export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -18,6 +19,7 @@ export const Home = () => {
       <HeroSection />
       <AboutSection />
       <SkillsSection />
+      <ProjectsSection />
     </main>
     {/* Footer */}
   </div>
